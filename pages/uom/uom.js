b022d637a68531292d16fa2a5a0c0485d310224a
Jingyu Hou <jerryhou@Jingyus-MacBook-Pro.local>
2019-05-21 21:01:39 +1000

    Optimisation

--- pages/uom/uom.js
+++ pages/uom/uom.js
@@ -6,22 +6,30 @@ Page({
         subjects: [
       {
         code: "comp10001",
-        name: "Foundation of Computing"
+        name: "Foundation of Computing",
+        content: "Some bullshit and balabalablabalbalblablaalb",
+        assig: "40%",
+        exam: "60%"
       },
       {
         code: "comp10002",
-        name: "Foundation of Algorithms"
+        name: "Foundation of Algorithms",
+        content: "I havent seen this before, please shut up",
+        assig: "30%",
+        exam: "70%"
       },
       {
         code: "comp20007",
-        name: "Design of Algorithm"
+        name: "Deisgn of algorithm",
+        content: "What is wrong with this bug No question mark ok", //不能有问号，转不好json
+        assig: "20%",
+        exam: "80%"
       }
-
     ]
   },
 
   tapSubject:function(event){
-    console.log(event)
+    console.log(JSON.stringify(this.data.subjects[event.currentTarget.id]))
     wx.navigateTo({
       url: '../subject_detail/subject_detail?subjects=' + JSON.stringify(this.data.subjects[event.currentTarget.id]),
     })
