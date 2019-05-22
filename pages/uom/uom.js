cec902201aa040c423ee3dc9097d590f22580e8c
Jingyu Hou <jerryhou@211.100.96.58.static.exetel.com.au>
2019-05-22 10:58:25 +1000

    Optimise the code, add the feature of mindkey

--- pages/uom/uom.js
+++ pages/uom/uom.js
@@ -1,9 +1,11 @@
 // pages/uom/uom.js
+
+var WxSearch = require('../searchbar/searchbar.js');
 Page({
   data: {
     inputShowed: false,
     inputVal: "",
-        subjects: [
+    subjects: [
       {
         code: "comp10001",
         name: "Foundation of Computing",
@@ -28,13 +30,13 @@ Page({
     ]
   },
 
-  tapSubject:function(event){
+  tapSubject: function (event) {
     console.log(JSON.stringify(this.data.subjects[event.currentTarget.id]))
     wx.navigateTo({
       url: '../subject_detail/subject_detail?subjects=' + JSON.stringify(this.data.subjects[event.currentTarget.id]),
     })
   },
-  
+
   showInput: function () {
     this.setData({
       inputShowed: true
@@ -55,5 +57,40 @@ Page({
     this.setData({
       inputVal: e.detail.value
     });
+  },
+
+  onLoad: function () {
+    var that = this;
+    WxSearch.init(
+      that,  // 本页面一个引用
+      ['comp10001', 'comp10002', 'comp20007', "comp30023", "swen20003, info20003"],// 搜索匹配，[]表示不使用
+      that.mySearchFunction, // 提供一个搜索回调函数
+      that.myGobackFunction //提供一个返回回调函数
+    );
+  },
+
+  // 转发函数,固定部分
+  wxSearchInput: WxSearch.wxSearchInput,  // 输入变化时的操作
+  wxSearchKeyTap: WxSearch.wxSearchKeyTap,  // 点击提示或者关键字、历史记录时的操作
+  wxSearchDeleteAll: WxSearch.wxSearchDeleteAll, // 删除所有的历史记录
+  wxSearchConfirm: WxSearch.wxSearchConfirm,  // 搜索函数
+  wxSearchClear: WxSearch.wxSearchClear,  // 清空函数
+
+  // 搜索回调函数  
+  mySearchFunction: function (value) {
+    // do your job here
+    // 跳转
+    wx.redirectTo({
+      url: '../index/index?searchValue=' + value
+    })
+  },
+
+  // 返回回调函数
+  myGobackFunction: function () {
+    // do your job here
+    // 跳转
+    wx.redirectTo({
+      url: '../index/index?searchValue=返回'
+    })
   }
 })
