aaf8d4df2fd905e4565b83be98e11e8205cec25b
Jingyu Hou <jerryhou@jingyus-mbp.home>
2019-05-25 11:18:05 +1000

    UI adapts various phone sizes

--- pages/comment/comment.js
+++ pages/comment/comment.js
@@ -11,85 +11,113 @@ Page({
     index: 0,
     maxLen: 200, // 最多放多少字
     info: "",
-    nowLen: 0,//备注当前字数
+    nowLen: 0, //备注当前字数
     flag_star: 0,
-    flag_emoji:0,
+    flag_emoji: 0,
   },
 
-  bindPickerChange: function (e) {
+  bindPickerChange: function(e) {
     console.log('携带值', e.detail.value)
     this.setData({
       index: e.detail.value
     })
   },
-  bindTextAreaChange: function (e) {
+  bindTextAreaChange: function(e) {
     var that = this
     var value = e.detail.value,
       len = parseInt(value.length);
     if (len > that.data.maxLen)
       return;
-    that.setData({ info: value, nowLen: len })
+    that.setData({
+      info: value,
+      nowLen: len
+    })
   },
 
-  changeColor1: function () {
+  changeColor1: function() {
     var that = this;
     that.setData({
       flag_star: 1
     });
   },
-  changeColor2: function () {
+  changeColor2: function() {
     var that = this;
     that.setData({
       flag_star: 2
     });
   },
-  changeColor3: function () {
+  changeColor3: function() {
     var that = this;
     that.setData({
       flag_star: 3
     });
   },
-  changeColor4: function () {
+  changeColor4: function() {
     var that = this;
     that.setData({
       flag_star: 4
     });
   },
-  changeColor5: function () {
+  changeColor5: function() {
     var that = this;
     that.setData({
       flag_star: 5
     });
   },
 
-  changeEmoji1: function () {
+  changeEmoji1: function() {
     var that = this;
     that.setData({
       flag_emoji: 1
     });
   },
-  changeEmoji2: function () {
+  changeEmoji2: function() {
     var that = this;
     that.setData({
       flag_emoji: 2
     });
   },
-  changeEmoji3: function () {
+  changeEmoji3: function() {
     var that = this;
     that.setData({
       flag_emoji: 3
     });
   },
-  changeEmoji4: function () {
+  changeEmoji4: function() {
     var that = this;
     that.setData({
       flag_emoji: 4
     });
   },
-  changeEmoji5: function () {
+  changeEmoji5: function() {
     var that = this;
     that.setData({
       flag_emoji: 5
     });
   },
+
+  submit: function(event) {
+    var that = this;
+    wx.showToast({
+      title: '发布成功',
+      icon: 'success',
+      duration: 1500,
+      mask: false,
+      success: function() {
+        that.setData({
+          info: '',
+          noteNowLen: 0,
+          flag: 0
+        })
+
+      }
+    })
+
+    setTimeout(function(){
+      wx.navigateBack({
+        delta: 2
+      })
+    }, 1500)
+
+  }
 })
